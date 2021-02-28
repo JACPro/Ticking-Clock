91ca9707675b2fc3e92f64fcb2348b7f36239c2f
J Cunningham <jamesacpro@gmail.com>
2021-02-28 23:20:20 +0000

    Created clock object with hour/minute/second hands and hour indicators

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    [SerializeField] Transform hoursPivot, minutesPivot, secondsPivot;
+
+    void Awake() {
+            
+    }
+}
